dbaf0cc496204fceed65fabf5312b020d49fba74
Shashi <shashi@kpis.in>
2024-12-05 23:12:55 +0530

    OtpLess otp apis

--- src/main/java/com/bt/ecommerce/primary/controller/customer/OneTimePasswordController.java
+++ src/main/java/com/bt/ecommerce/primary/controller/customer/OneTimePasswordController.java
@@ -2,18 +2,20 @@ package com.bt.ecommerce.primary.controller.customer;
 
 import com.bt.ecommerce.annotation.TranslateResponseMessage;
 import com.bt.ecommerce.bean.ResponsePacket;
-import com.bt.ecommerce.configuration.SpringBeanContext;
 import com.bt.ecommerce.exception.BadRequestException;
 import com.bt.ecommerce.primary.controller._BaseController;
 import com.bt.ecommerce.primary.dto.CustomerDto;
-import com.bt.ecommerce.primary.pojo.user.Customer;
-import com.bt.ecommerce.security.JwtUserDetailsService;
-import jakarta.servlet.http.HttpServletRequest;
+import com.bt.ecommerce.primary.dto.OtpDto;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -27,6 +29,26 @@ public class OneTimePasswordController extends _BaseController {
         return new ResponseEntity<>(ResponsePacket.builder()
                 .errorCode(0)
                 .message("ecommerce.common.message.otp_sent")
+                .build(), HttpStatus.OK);
+    }
+
+    @TranslateResponseMessage
+    @PostMapping("/sendOtpUsingOtpLess")
+    public ResponseEntity<ResponsePacket> sendOtpUsingOtpLess(@RequestBody OtpDto.OTPLess otpBean) throws BadRequestException, IOException {
+        oneTimePasswordService.sendOtpUsingOtpLess(otpBean.getIsdCode(), otpBean.getPhoneNumber());
+        return new ResponseEntity<>(ResponsePacket.builder()
+                .errorCode(0)
+                .message("ecommerce.common.message.")
+                .build(), HttpStatus.OK);
+    }
+
+    @TranslateResponseMessage
+    @PostMapping("/verifyOtpUsingOtpLess")
+    public ResponseEntity<ResponsePacket> verifyOtpUsingOtpLess(@RequestBody OtpDto.OTPVerify otpVerify) throws BadRequestException, IOException {
+        oneTimePasswordService.verifyOtpUsingOtpLess(otpVerify);
+        return new ResponseEntity<>(ResponsePacket.builder()
+                .errorCode(0)
+                .message("ecommerce.common.message.")
                 .build(), HttpStatus.OK);
     }
 
