bf5d230f6c015f28c57c0074749b34537f47df50
Ajit Singh Rathore <ajit.rathore@trigyn.com>
2025-01-10 15:47:07 +0530

    1.Added @RestController annotation in url controller

--- src/main/java/com/bt/ecommerce/primary/controller/UrlController.java
+++ src/main/java/com/bt/ecommerce/primary/controller/UrlController.java
@@ -6,11 +6,11 @@ import com.bt.ecommerce.exception.BadRequestException;
 import com.bt.ecommerce.utils.Const;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UrlController {
 
     @TranslateResponseMessage
